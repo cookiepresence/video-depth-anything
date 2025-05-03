14fd2a0c15fde0d4c46d87197dcd63d864361336
rogerthat-0420 <kushang.agarwal@students.iiit.ac.in>
2025-05-03 14:52:29 +0530

    added code to check the output with the replacing of backbone in VDA with finetuned model

--- run.py
+++ run.py
@@ -10,7 +10,9 @@
 # distributed under the License is distributed on an "AS IS" BASIS, 
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 # See the License for the specific language governing permissions and 
-# limitations under the License. 
+# limitations under the License.
+from typing import Optional
+import pathlib
 import argparse
 import numpy as np
 import os
@@ -21,17 +23,20 @@ from utils.dc_utils import read_video_frames, save_video
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Video Depth Anything')
-    parser.add_argument('--input_video', type=str, default='./assets/example_videos/davis_rollercoaster.mp4')
-    parser.add_argument('--output_dir', type=str, default='./outputs')
-    parser.add_argument('--input_size', type=int, default=518)
-    parser.add_argument('--max_res', type=int, default=1280)
+    parser.add_argument('--input-video', type=str, default='./assets/example_videos/davis_rollercoaster.mp4')
+    parser.add_argument('--output-dir', type=str, default='./outputs')
+    parser.add_argument('--input-size', type=int, default=518)
+    parser.add_argument('--max-res', type=int, default=1280)
     parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitl'])
-    parser.add_argument('--max_len', type=int, default=-1, help='maximum length of the input video, -1 means no limit')
-    parser.add_argument('--target_fps', type=int, default=-1, help='target fps of the input video, -1 means the original fps')
+    parser.add_argument('--max-len', type=int, default=-1, help='maximum length of the input video, -1 means no limit')
+    parser.add_argument('--target-fps', type=int, default=-1, help='target fps of the input video, -1 means the original fps')
     parser.add_argument('--fp32', action='store_true', help='model infer with torch.float32, default is torch.float16')
     parser.add_argument('--grayscale', action='store_true', help='do not apply colorful palette')
-    parser.add_argument('--save_npz', action='store_true', help='save depths as npz')
-    parser.add_argument('--save_exr', action='store_true', help='save depths as exr')
+    parser.add_argument('--save-npz', action='store_true', help='save depths as npz')
+    parser.add_argument('--save-exr', action='store_true', help='save depths as exr')
+
+    # load backbone from different model
+    parser.add_argument('--backbone-path', type=pathlib.Path, help='load [DepthAnything] backbone from different path?')
 
     args = parser.parse_args()
 
@@ -43,7 +48,23 @@ if __name__ == '__main__':
     }
 
     video_depth_anything = VideoDepthAnything(**model_configs[args.encoder])
-    video_depth_anything.load_state_dict(torch.load(f'./checkpoints/video_depth_anything_{args.encoder}.pth', map_location='cpu'), strict=True)
+    vda_weights = torch.load(f'./checkpoints/video_depth_anything_{args.encoder}.pth', map_location='cpu', weights_only=True)
+    if args.backbone_path:
+        # ensures that we share the same backbone for pretrained and finetuned models
+        da_pretrained_weights = torch.load(args.backbone_path, map_location='cpu', weights_only=True)
+        for k, v in da_pretrained_weights.items():
+            if 'pretrained' in k:
+                vda_weight = video_depth_anything.state_dict()[k]
+                # print(f"replacing {k} in vda: before -- {torch.linalg.norm(vda_weight, dim=0, ord=2)}, after -- {torch.linalg.norm(v, dim=0, ord=2)}")
+                vda_weights[k] = v
+            elif 'head' in k:
+                # ensure that we keep the names the same
+                k = k.replace('depth_head', 'head')
+                vda_weight = video_depth_anything.state_dict()[k]
+                # print(f"replacing {k} in vda: before -- {torch.linalg.norm(vda_weight, dim=0, ord=2)}, after -- {torch.linalg.norm(v, dim=0, ord=2)}")
+                vda_weights[k] = v
+        print("replaced video depth anything backbone with depthanything!")
+        video_depth_anything.load_state_dict(vda_weights, strict=True)
     video_depth_anything = video_depth_anything.to(DEVICE).eval()
 
     frames, target_fps = read_video_frames(args.input_video, args.max_len, args.target_fps, args.max_res)
@@ -75,7 +96,4 @@ if __name__ == '__main__':
             exr_file = OpenEXR.OutputFile(output_exr, header)
             exr_file.writePixels({"Z": depth.tobytes()})
             exr_file.close()
-
-    
-
 
