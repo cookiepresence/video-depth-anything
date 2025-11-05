e95a9512234d263cfdc137ea6c592c308c432fc2
cookiepresence <127502417+cookiepresence@users.noreply.github.com>
2025-11-05 16:13:06 +0530

    feat: add proc depth eval

---
+++ scripts/download_proc-depth-eval.sh
@@ -0,0 +1,11 @@
+ROOT=/scratch/mde/proc-depth-eval/
+
+mkdir -p /scratch/mde
+mkdir -p $ROOT
+
+cd $ROOT
+
+# download the dataset
+uvx gdown --fuzzy "1xJa2UX77Wit1EZsNV0ZzxvQfywCr61HP" --continue
+
+cd -
